3905b0d85103ab682be98ebca4fb3287311b4945
kapilkariya <kapilkariya1105@gmail.com>
2026-05-18 08:37:40 +0530

    temp

--- temp.java
+++ temp.java
@@ -1,5 +1,20 @@
-public class temp{
-  public static void main(String args[]){
-    
-  }
+import java.util.*;
+
+public class temp {
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+
+        int n = sc.nextInt();
+
+        int fact = 1;
+
+        for(int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+
+        System.out.println("Factorial = " + fact);
+    }
 }
